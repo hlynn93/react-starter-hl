e518cb3dc88b13eb264598fe710e45bd872f6fa5
Han <han@logicalsteps.net>
2018-06-06 12:14:23 +0800

    clean up files

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 
-import './index.css';
 import store, { history } from './store';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
